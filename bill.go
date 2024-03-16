80344cb18a1b1795c085ab69b0f744897a8f0464
Qais Wardag <qais.wardag@outlook.com>
2024-03-16 20:37:52 +0100

    Add feature for adding items and tip

--- bill.go
+++ bill.go
@@ -11,11 +11,9 @@ type bill struct {
 // make new bills
 func newBill(name string) bill {
 	billOjbect := bill{
-		name: name,
-		items: map[string]float64{
-			"pie": 5.99, "cake": 3.99,
-		},
-		tip: 0,
+		name:  name,
+		items: map[string]float64{},
+		tip:   0,
 	}
 
 	return billOjbect
@@ -24,20 +22,33 @@ func newBill(name string) bill {
 // Receiver Function
 // this function can only be called from bill object only
 // So the function is not a function on its own
-func (b bill) formatBill() string {
+func (b *bill) formatBill() string {
 	formatString := "Bill breakdown: \n"
 
 	var total float64 = 0
 
 	// list items
 	for k, v := range b.items {
-		formatString += fmt.Sprintf("%-25v $%v \n", k+":", v)
+		formatString += fmt.Sprintf("%-40v $%v \n", k+":", v)
 
 		total += v
 	}
 
+	// tip
+	formatString += fmt.Sprintf("%-40v $%v\n\n", "Tip:", b.tip)
+
 	// total
-	formatString += fmt.Sprintf("%-25v $%0.2f", "total:", total)
+	formatString += fmt.Sprintf("%-40v $%0.2f", "Total:", total+b.tip)
 
 	return formatString
+}
+
+// update tip
+func (b *bill) updateTip(tip float64) {
+	(*b).tip = tip
+}
+
+// add an item to bill
+func (b *bill) addItem(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 
 	theBill := newBill("mario's bill")
+	theBill.updateTip(10)
+
+	theBill.addItem("Fried Egg", 8)
+	theBill.addItem("Steak with Avocado", 20)
+	theBill.addItem("Ice cream", 10)
 
 	fmt.Println(theBill.formatBill())
 }
